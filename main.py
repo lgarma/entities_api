8ae98eb1a3659d6a1d21b53575caf5dcb830ba96
lgarma <garmahds@gmail.com>
2023-10-13 15:27:28 -0600

    feat: Clase con pydantic para validación y ejemplos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """API for entity recognition using the spacy library."""
 from typing import Any
-
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
 import spacy
 
@@ -9,19 +9,28 @@ app = FastAPI()
 nlp = spacy.load("es_core_news_sm")
 
 
+class SentencesRequest(BaseModel):
+    """Request model for the get_entities endpoint.
+
+    Attributes:
+        oraciones (list[str]): List of sentences to be processed by spacy.
+    """
+    oraciones: list[str] = Field(..., example=["Apple está buscando comprar una startup del Reino Unido por mil millones de dólares.",
+                                                  "San Francisco considera prohibir los robots de entrega en la acera."])
+
 @app.post("/entities/")
-def get_entities(sentences: dict[str, list[str]]) -> dict[str, Any]:
-    """Return the named entities given a list of sentences.
+def get_entities(sentences: SentencesRequest) -> dict[str, Any]:
+    """Get named entities from a list of sentences.
 
     Parameters:
-        sentences (Sentences):
+        sentences (dict[str, list[str]]):
         List of sentences to be processed by spacy.
 
     Returns:
         dict[str, Any]: Dictionary with the sentences and their entities.
     """
     response = []
-    for sentence in sentences["oraciones"]:
+    for sentence in sentences.oraciones:
         sentence_response = {"oración": sentence, "entidades": {}}
         doc = nlp(sentence)
         for ent in doc.ents:
